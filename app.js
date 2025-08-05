1281c52bd3a89d1ea944c8403971b3fa4204bf86
Duckdrunk <brayanramirezsanchez17@gmail.com>
2025-08-04 22:30:03 -0500

    Función agregar amigos

--- app.js
+++ app.js
@@ -4,3 +4,15 @@ const inputNombre = document.getElementById("amigo");
 const listaAmigos = document.getElementById("listaAmigos");
 const resultado = document.getElementById("resultado");
 
+function agregarAmigo() {
+    const nombre = inputNombre.value.trim();
+
+    if (!nombre) {
+        mostrarAlerta("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    inputNombre.value = "";
+    mostrarAmigos();
+}
