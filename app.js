34f132380b27ae1dd2ec4a6c1bc46d4bb6d0b294
Duckdrunk <brayanramirezsanchez17@gmail.com>
2025-08-04 22:17:28 -0500

    Array para amigos y constantes

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+const inputNombre = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+const resultado = document.getElementById("resultado");
+
